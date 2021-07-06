44dd36c734d10f7407c24087bf61c7e574c5a741
davedwight <dpdwight@gmail.com>
2021-07-05 22:35:38 -0600

    All requests working

--- api/server.js
+++ api/server.js
@@ -6,6 +6,25 @@ const server = express();
 
 server.use(express.json());
 
+server.post('/api/users', (req, res) => {
+
+    const { name, bio } = req.body;
+
+    if (!name || !bio) {
+        res.status(400).json({ message: "Please provide name and bio for the user" })
+    } else {
+        User.insert({ name, bio })
+            .then(user => {
+                res.status(201).json(user)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "There was an error while saving the user to the database"
+                })
+            })
+    }
+})
+
 server.get('/api/users', (req, res) => {
     // res.status(200).json({ message: 'fetching all users'})
     User.find()
@@ -41,19 +60,51 @@ server.get('/api/users/:id', (req, res) => {
         })
 })
 
-server.post('/api/users', (req, res) => {
-    console.log(req.body.name);
-    console.log(req.body.bio);
-})
-
 server.delete('/api/users/:id', (req, res) => {
+   
     const { id } = req.params;
-    res.status(200).json({ message: `deleting user with id ${id}`})
+
+    User.remove(id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: `user with id ${id} does not exist`
+                })
+            } else {
+                res.json(user)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: err.message,
+            })
+        })
 })
 
 server.put('/api/users/:id', (req, res) => {
     const { id } = req.params;
-    res.status(201).json({ message: `updating user with id: ${id}`})
+    const { name, bio } = req.body;
+    if(!name || !bio) {
+        res.status(422).json({
+            message: 'name and bio are required'
+        })
+    } else {
+        User.update(id, {name, bio})
+            .then(updated => {
+                if (!updated) {
+                    res.status(404).json({
+                        message: `user with id ${id} not found`
+                    })
+                } else {
+                    res.json(updated)
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: err.message
+                })
+            })
+    }
 })
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
