1006f551353896aa8a9094583d3616b749dcb546
davedwight <dpdwight@gmail.com>
2021-07-05 19:52:52 -0600

    All requests scaffolded

--- api/server.js
+++ api/server.js
@@ -1,3 +1,59 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const User = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    // res.status(200).json({ message: 'fetching all users'})
+    User.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: err.message,
+            })
+        })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    // res.status(200).json({ message: 'fetching all users'})
+    
+    const { id } = req.params;
+
+    User.findById(id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: `user with id ${id} does not exist`
+                })
+            } else {
+                res.status(200).json(user)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: err.message,
+            })
+        })
+})
+
+server.post('/api/users', (req, res) => {
+    console.log(req.body.name);
+    console.log(req.body.bio);
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    res.status(200).json({ message: `deleting user with id ${id}`})
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    res.status(201).json({ message: `updating user with id: ${id}`})
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
